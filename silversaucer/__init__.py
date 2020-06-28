ecff93e6b8a0021ba7a76a5b1e23352b5b76a657
Paul Cutler <paul.r.cutler@gmail.com>
2020-06-28 18:50:18 -0500

    Update views
    Create home controller to control the home and about
    pages and remove the default.py views file

--- silversaucer/__init__.py
+++ silversaucer/__init__.py
@@ -4,8 +4,16 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    with Configurator(settings=settings) as config:
-        config.include('pyramid_chameleon')
-        config.include('.routes')
-        config.scan()
+    config = Configurator(settings=settings)
+    config.include('pyramid_chameleon')
+    init_routing(config)
     return config.make_wsgi_app()
+
+
+def init_routing(config):
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('play', '/play')
+    config.add_route('today', '/today')
+    config.add_route('about', '/about')
+    config.scan()
