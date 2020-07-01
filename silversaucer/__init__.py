d2e87d9f9cbc403ad3fb2b9917a648c690b625fe
Paul Cutler <paul.r.cutler@gmail.com>
2020-06-30 21:37:55 -0500

    Add 7" play option
    Add a smaller icon to play a single or EP; add the
    layout page; add its routing

--- silversaucer/__init__.py
+++ silversaucer/__init__.py
@@ -14,6 +14,7 @@ def init_routing(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('play', '/play')
+    config.add_route('play-single', '/play-single')
     config.add_route('today', '/today')
     config.add_route('about', '/about')
     config.scan()
--- silversaucer/controllers/music_controller.py
+++ silversaucer/controllers/music_controller.py
@@ -6,6 +6,11 @@ def play(_):
     return {}
 
 
+@view_config(route_name='play-single', renderer='silversaucer:templates/play/play-single.pt')
+def play(_):
+    return {}
+
+
 @view_config(route_name='today', renderer='silversaucer:templates/today/today.pt')
 def today(_):
     return {}
