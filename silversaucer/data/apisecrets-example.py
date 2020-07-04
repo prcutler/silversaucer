2a22841c651e720a045db94561d8692d5e1ca388
Paul Cutler <paul.r.cutler@gmail.com>
2020-07-04 09:45:33 -0500

    Add placeholder for app key and secret
    See https://www.discogs.com/developers/#page:authentication - will be needed for image GET requests

--- silversaucer/data/apisecrets-example.py
+++ silversaucer/data/apisecrets-example.py
@@ -3,4 +3,7 @@
 discogs_user_token = ""
 discogs_app_name = "SilverSaucer/0.1"
 
+discogs_key = ""
+discogs_secret = ""
+
 discogs_url = "https://api.discogs.com/"
--- silversaucer/services/play_service.py
+++ silversaucer/services/play_service.py
@@ -1,3 +1,4 @@
+import requests
 
 
 def randomize_record():
