d219c8469fe307ac47a71e5d27ecdc3f7e9aafa3
Paul Cutler <paul.r.cutler@gmail.com>
2020-07-12 11:33:32 -0500

    Add method for EP collection (12", 10" and 7" records)

--- silversaucer/services/play_service.py
+++ silversaucer/services/play_service.py
@@ -6,7 +6,7 @@ import random
 # Discogs API Url for different folders in a collection
 discogs_url = config.discogs_url + "/users/" + config.discogs_user + "/collection/folders/"
 
-# If you have not put your records into folder, use all_discogs or enter the folder ID for your folders below
+# If you have not put your records into folders, use all_discogs or enter the folder ID for your folders below
 # (I have created folders for each type in my collection)
 
 all_discogs = 0
@@ -30,9 +30,33 @@ def get_lp_collection():
     return random_lp
 
 
-# Includes both 7" and EP folders
+# Includes 7", 12 and EP folders
 def get_ep_collection():
-    pass
+
+    random_folder = random.randint(0, 2)
+
+    if random_folder == 0:
+        response = requests.get(discogs_url + twelve_inch_folder)
+
+        record_json = response.json()
+        ep_count = int(record_json["count"])
+        random_ep = random.randint(1, ep_count)
+
+    elif random_folder == 1:
+        response = requests.get(discogs_url + ten_inch_folder)
+
+        record_json = response.json()
+        ep_count = int(record_json["count"])
+        random_ep = random.randint(1, ep_count)
+
+    else:
+        response = requests.get(discogs_url + seven_inch_folder)
+
+        record_json = response.json()
+        ep_count = int(record_json["count"])
+        random_ep = random.randint(1, ep_count)
+
+    return random_ep
 
 
 def get_cd_collection():
