0b4275fc60574942a96a0c9b9631596b05be5d1e
Paul Cutler <paul.r.cutler@gmail.com>
2020-07-12 11:04:32 -0500

    Refactor play_service.py to include Discogs folders as variables for methods
    List all folders and their specific ID in Discogs (for my Discogs user).
    Re-write method to use these variables instead

--- silversaucer/services/play_service.py
+++ silversaucer/services/play_service.py
@@ -6,10 +6,22 @@ import random
 # Discogs API Url for different folders in a collection
 discogs_url = config.discogs_url + "/users/" + config.discogs_user + "/collection/folders/"
 
+# If you have not put your records into folder, use all_discogs or enter the folder ID for your folders below
+# (I have created folders for each type in my collection)
+
+all_discogs = 0
+lp_folder = "/2162484/"
+twelve_inch_folder = "/2198941/"
+ten_inch_folder = "/2162486/"
+seven_inch_folder = "/2162483/"
+cd_folder = "/2162488/"
+tape_folder = "/2162487/"
+digital_folder = "/2198943/"
+
 
 # Get just full length vinyl records in my LP folder in my collection - replace your folder # in the response method
 def get_lp_collection():
-    response = requests.get(discogs_url + "2162484")
+    response = requests.get(discogs_url + lp_folder)
 
     record_json = response.json()
     lp_count = int(record_json["count"])
@@ -31,5 +43,3 @@ def get_tape_collection():
     pass
 
 
-def randomize_record():
-    pass
