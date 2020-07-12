3d48cb95bfa82806f356dcd0728c5489b7bbef2d
Paul Cutler <paul.r.cutler@gmail.com>
2020-07-12 10:53:59 -0500

    Refactor play_service.py so each method can use discogs_url
    I think that's even pythonic so each method doesn't have to type that all out

--- silversaucer/services/play_service.py
+++ silversaucer/services/play_service.py
@@ -1,11 +1,24 @@
 import requests
 import os
+import silversaucer.data.config as config
+import random
+
+# Discogs API Url for different folders in a collection
+discogs_url = config.discogs_url + "/users/" + config.discogs_user + "/collection/folders/"
 
 
-def get_vinyl_collection():
-    pass
+# Get just full length vinyl records in my LP folder in my collection - replace your folder # in the response method
+def get_lp_collection():
+    response = requests.get(discogs_url + "2162484")
+
+    record_json = response.json()
+    lp_count = int(record_json["count"])
+    random_lp = random.randint(0, lp_count)
+
+    return random_lp
 
 
+# Includes both 7" and EP folders
 def get_ep_collection():
     pass
 
