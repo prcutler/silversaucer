469d0dbb28460160b41df181d644156f931a802c
Paul Cutler <paul.r.cutler@gmail.com>
2020-07-12 09:54:01 -0500

    Add stubs for methods to get different folders on Discogs

--- silversaucer/services/play_service.py
+++ silversaucer/services/play_service.py
@@ -1,4 +1,21 @@
 import requests
+import os
+
+
+def get_vinyl_collection():
+    pass
+
+
+def get_ep_collection():
+    pass
+
+
+def get_cd_collection():
+    pass
+
+
+def get_tape_collection():
+    pass
 
 
 def randomize_record():
