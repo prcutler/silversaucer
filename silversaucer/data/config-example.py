0aead5d44731fb9648ac0c544c244e54fb1b5a6f
Paul Cutler <paul.r.cutler@gmail.com>
2020-07-12 10:43:04 -0500

    Update config-example.py with all needed fields
    Rename discogs_app_name to discogs_user_agent to be
    consistent with Postman.  Add oauth_token_secret, verifier and token to config file

--- silversaucer/data/config-example.py
+++ silversaucer/data/config-example.py
@@ -1,10 +1,16 @@
+import os
 
 # Discogs
 discogs_user_token = ""
-discogs_app_name = "SilverSaucer/0.1"
+discogs_user_agent = "SilverSaucer/0.1"
 
-# Only use this if you cannot use environment variables (recommended)
+# Only use this if you cannot use environment variables (which are recommended)
 discogs_key = ""
 discogs_secret = ""
+discogs_oauth_token_secret = ""
+discogs_oauth_verifier = ""
+discogs_oauth_token = ""
 
 discogs_url = "https://api.discogs.com/"
+
+discogs_user = "prcutler"
