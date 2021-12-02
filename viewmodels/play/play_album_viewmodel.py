897acd5eaf6161ef55324d92fb7b5dc1f19e8ffc
Paul Cutler <paul@paulcutler.org>
2021-12-02 16:38:45 -0600

    Update viewmodels with corre service

--- viewmodels/play/play_album_viewmodel.py
+++ viewmodels/play/play_album_viewmodel.py
@@ -2,6 +2,7 @@ from typing import List
 
 from starlette.requests import Request
 
+from services import RandomRecordService
 from viewmodels.shared.viewmodel import ViewModelBase
 
 
@@ -9,9 +10,15 @@ class PlayAlbumViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
 
-        self.release_uri: str
-        self.release_image_uri: str
-        self.release_title: str
-        self.release_date: str
-        self.release_date: int
-        self.genres: str
+        self.release_title: str = RandomRecordService.get_album_data()
+        self.release_uri: str = RandomRecordService.get_album_data()
+        self.artist_name: str = RandomRecordService.get_album_data()
+        self.artist_url: str = RandomRecordService.get_album_data()
+        self.release_image_uri: str = RandomRecordService.get_album_data()
+        self.release_title: str = RandomRecordService.get_album_data()
+        self.release_data: str = RandomRecordService.get_album_data()
+        self.release_data: int = RandomRecordService.get_album_data()
+        self.genres: str = RandomRecordService.get_album_data()
+        self.discogs_main_id: int = RandomRecordService.get_album_data()
+        self.discogs_main_url: str = RandomRecordService.get_album_data()
+        self.main_release_date: int = RandomRecordService.get_album_data()
