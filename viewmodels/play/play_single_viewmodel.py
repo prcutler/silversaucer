005ad246f8d82a72007f2bd1cdcebea18dc3207b
Paul Cutler <paul@paulcutler.org>
2021-12-02 15:56:14 -0600

    Update viewmodels - now working

--- viewmodels/play/play_single_viewmodel.py
+++ viewmodels/play/play_single_viewmodel.py
@@ -9,3 +9,10 @@ from viewmodels.shared.viewmodel import ViewModelBase
 class PlaySingleViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
+
+        self.release_uri: str
+        self.release_image_uri: str
+        self.release_title: str
+        self.release_date: str
+        self.release_date: str
+        self.genres: str
--- views/play.py
+++ views/play.py
@@ -4,9 +4,11 @@ import fastapi
 from fastapi_chameleon import template
 from starlette.requests import Request
 
+from services.play_service import RandomRecordService
 from viewmodels.play.play_album_viewmodel import PlayAlbumViewModel
 from viewmodels.play.play_single_viewmodel import PlaySingleViewModel
 from viewmodels.play.play_viewmodel import PlayViewModel
+from viewmodels.shared.viewmodel import ViewModelBase
 
 router = fastapi.APIRouter()
 
@@ -15,17 +17,17 @@ router = fastapi.APIRouter()
 @template(template_file="")
 def play(request: Request):
     vm = PlayViewModel(request)
-    return {}
+    return vm.to_dict()
 
 
 @router.get("/play/play-album")
 @template(template_file="play/play-album.pt")
 def playalbum(request: Request):
     vm = PlayAlbumViewModel(request)
-    # folder = 2162484
-    # album_release_id = RandomRecordService.get_folder_count(folder)
-    # release_data = RandomRecordService.get_album_data(folder, album_release_id)
-    return {}
+    folder = 2162484
+    album_release_id = RandomRecordService.get_folder_count(folder)
+    release_data = RandomRecordService.get_album_data(folder, album_release_id)
+    return vm.to_dict()
 
 
 @router.get("/play/play-single")
@@ -44,5 +46,4 @@ def playsingle(request: Request):
 
     # album_release_id = RandomRecordService.get_folder_count(single)
     # release_data = RandomRecordService.get_album_data(folder, album_release_id)
-
-    return {}
+    return vm.to_dict()
