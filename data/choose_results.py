dd5e66c6e32ceb6d53f01032e70e8d720ec338ad
Paul Cutler <paul@paulcutler.org>
2021-12-03 14:57:19 -0600

    Refactor to add 2nd choice page for results

---
+++ data/choose_results.py
@@ -0,0 +1,20 @@
+class ChooseResultsInfo:
+    def __init__(
+        self,
+        folder: str,
+        folder_number: int,
+        release_title: str,
+        artist_name: str,
+        release_date: str,
+        genres: str,
+        main_release_date: int,
+    ):
+
+        self.artist_name = artist_name
+        self.folder = folder
+        self.folder_number = folder_number
+        self.release_title = release_title
+        self.artist_name = artist_name
+        self.release_date = release_date
+        self.genres = genres
+        self.main_release_date: int = main_release_date
