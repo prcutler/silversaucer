4c3df7aaf5ea043aed84522c753aead3f94ab01f
Paul Cutler <paul@paulcutler.org>
2021-12-04 07:26:50 -0600

    Fix missing import - site works!

--- views/home.py
+++ views/home.py
@@ -1,5 +1,6 @@
 import fastapi
 from fastapi_chameleon import template
+from starlette.requests import Request
 
 from viewmodels.home.indexviewmodel import IndexViewModel
 from viewmodels.shared.viewmodel import ViewModelBase
@@ -8,12 +9,14 @@ router = fastapi.APIRouter()
 
 
 @router.get("/")
-@template(template_file="")
-def index():
-    return
+@template()
+def index(request: Request):
+    vm = IndexViewModel(request)
+    return vm.to_dict()
 
 
 @router.get("/about")
-@template(template_file="home/about.pt")
-def about():
-    return {}
+@template()
+def about(request: Request):
+    vm = ViewModelBase(request)
+    return vm.to_dict()
