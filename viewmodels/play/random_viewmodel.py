36c1ce49484b9a1073bf872559184e94d06306af
Paul Cutler <paul@paulcutler.org>
2021-12-14 16:56:06 -0600

    Class inputs must match in order *@#$%

--- viewmodels/play/random_viewmodel.py
+++ viewmodels/play/random_viewmodel.py
@@ -2,7 +2,6 @@ from typing import List, Optional
 
 from starlette.requests import Request
 
-from data.album import AlbumInfo
 from services.play_service import RandomRecordService
 from viewmodels.shared.viewmodel import ViewModelBase
 
@@ -15,13 +14,13 @@ class RandomViewModel(ViewModelBase):
         album_release_id = RandomRecordService.get_folder_count(folder)
         print(album_release_id)
 
-        release_data = RandomRecordService.get_album_data(folder, album_release_id)
+        release_data = RandomRecordService.get_album_data(album_release_id)
 
         self.release_id = release_data.release_id
         self.release_uri: Optional[str] = release_data.release_uri
         self.artist_id = release_data.artist_id
-        self.release_title: Optional[str] = release_data.release_title
         self.artist_name: Optional[str] = release_data.artist_name
+        self.release_title: Optional[str] = release_data.release_title
         self.artist_url: Optional[str] = release_data.artist_url
         self.release_image_uri: Optional[str] = release_data.release_image_uri
         self.genres: Optional[List[str]] = release_data.genres
