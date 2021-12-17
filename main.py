f9740ce89eaf439681a413c5f1d96f12a46dd30f
Paul Cutler <paul@paulcutler.org>
2021-12-17 12:18:31 -0600

    Fix run call in main.py

--- main.py
+++ main.py
@@ -32,6 +32,6 @@ def configure_routes():
 
 if __name__ == "__main__":
     main()
-    uvicorn.run(api, port=8000, host="127.0.0.1")
+    uvicorn.run(app, port=8000, host="127.0.0.1")
 else:
     configure()
