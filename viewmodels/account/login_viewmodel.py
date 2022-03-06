f83eea9fb98a4dcc65f5ab3b4e145a28fff054b7
Paul Cutler <paul@paulcutler.org>
2022-03-06 17:16:53 -0600

    Add database and account functionality

--- viewmodels/account/login_viewmodel.py
+++ viewmodels/account/login_viewmodel.py
@@ -1,6 +1,5 @@
 from starlette.requests import Request
 
-import data.config as config
 from viewmodels.shared.viewmodel import ViewModelBase
 
 
@@ -8,21 +7,15 @@ class LoginViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
 
-        self.username = ""
-        self.password = ""
+        self.email = ''
+        self.password = ''
 
     async def load(self):
         form = await self.request.form()
-        self.username = form.get("username", "").lower().strip()
-        print(self.username)
-        self.password = form.get("password", "").strip()
+        self.email = form.get('email', '').lower().strip()
+        self.password = form.get('password', '').strip()
 
-        #        if not self.username():
-        #            self.error = "You must specify a username."
-        #        elif not self.password:
-        #            self.error = "You must specify a password."
-        if (
-            not self.username == config.username
-            and not self.password == config.password
-        ):
-            self.error = "You don't belong here.  Go away!"
+        if not self.email or not self.email.strip():
+            self.error = 'You must specify a email.'
+        elif not self.password:
+            self.error = 'You must specify a password.'
