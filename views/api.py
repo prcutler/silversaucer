747d3e326c2aefcbc965de602340659ca37fe2e6
Paul Cutler <paul@paulcutler.org>
2022-03-07 17:58:01 -0600

    Fix PEP8 new line

--- views/api.py
+++ views/api.py
@@ -7,7 +7,7 @@ router = fastapi.APIRouter()
 @router.get("/api/album")
 def today():
     return {
-        "album": "Today",
-        "artist": "The Beatles",
+        "album": "Like a Prayer",
+        "artist": "Madonna",
         "url": "https://i.discogs.com/6UaW3D3DWavkDhoC4JY5B4pVATBWNsZUBaUKRPqe_0A/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWltYWdlcy9SLTEw/ODc2NjMtMTI4OTA4/MjkyNC5qcGVn.jpeg"
     }
