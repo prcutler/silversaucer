400aadf0c161bf22a9ab6b53e2ec4ca5a430b6ad
Paul Cutler <paul@paulcutler.org>
2022-03-07 18:32:53 -0600

    Update JSON image for testing

--- views/api.py
+++ views/api.py
@@ -9,5 +9,5 @@ def today():
     return {
         "album": "Like a Prayer",
         "artist": "Madonna",
-        "url": "https://i.discogs.com/6UaW3D3DWavkDhoC4JY5B4pVATBWNsZUBaUKRPqe_0A/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWltYWdlcy9SLTEw/ODc2NjMtMTI4OTA4/MjkyNC5qcGVn.jpeg"
+        "url": "https://i.discogs.com/KiU4Bziov6339DcBKIvXaq9vYQQN9Gd95E39T3eIBmY/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQwNDQ0/OC0xNDU3MTk5ODMw/LTU4NzUuanBlZw.jpeg"
     }
