88e65a173a3db0e9529d8bb56e703d770c431482
Paul Cutler <paul@paulcutler.org>
2022-03-07 18:35:27 -0600

    Update JSON image for testing

--- views/api.py
+++ views/api.py
@@ -7,7 +7,7 @@ router = fastapi.APIRouter()
 @router.get("/api/album")
 def today():
     return {
-        "album": "Like a Prayer",
-        "artist": "Madonna",
+        "album": "Heaven is a Place on Earth",
+        "artist": "Belinda Carlisle",
         "url": "https://i.discogs.com/KiU4Bziov6339DcBKIvXaq9vYQQN9Gd95E39T3eIBmY/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQwNDQ0/OC0xNDU3MTk5ODMw/LTU4NzUuanBlZw.jpeg"
     }
