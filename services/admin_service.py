cefe5bcee823c73d8fbf8c54685e1ce97dd9fdfe
Paul Cutler <paul@paulcutler.org>
2022-05-29 10:30:41 -0500

    Stub out admin services

---
+++ services/admin_service.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from data.api_json import JSONData
+from data.album import AlbumInfo
+from sqlalchemy.future import select
+from data import db_session
+from data import config
+
+import requests
+
+
+async def get_db_data() -> dict:
+    pass
+
+async def update_db_data -> dict:
+    pass
