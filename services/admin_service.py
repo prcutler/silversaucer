091b6f30a96bfdf1117b476632a7aad116b77d04
Paul Cutler <paul@paulcutler.org>
2022-06-02 10:55:04 -0500

    Working db import and update

--- services/admin_service.py
+++ services/admin_service.py
@@ -6,6 +6,7 @@ from data import db_session
 from data import config
 import discogs_client
 
+
 import requests
 
 me = config.my_data.identity()
@@ -19,32 +20,52 @@ async def get_db_data():
     for records in me.collection_folders[8].releases:
         album_data = Album()
 
-        try:
+        async with db_session.create_async_session() as session:
+            release_id_query = select(Album.release_id).filter(Album.release_id == records.release.id)
+            results = await session.execute(release_id_query)
 
-            album_data.release_id = records.release.id
-            album_data.release_url = records.release.url
-            album_data.artist_id = records.release.artists[0].id
-            album_data.artist_id = None
-            album_data. artist_name = records.release.artists[0].name
-            album_data.release_title = records.release.title
-            album_data.artist_url = records.release.artists[0].url
-            album_data.release_image_url = records.release.images[0]["uri"]
-            #album_data.genres = records.release.genres
-            album_data.release_date = records.release.year
-            album_data.main_release_date = records.release.master.fetch("year")
-            album_data.discogs_main_id = records.release.master.id
-            album_data.folder = folder
+            release_id_results = results.scalar_one_or_none()
+            print("DB query results: ", release_id_results, "Type: ", type(release_id_results))
 
-        except discogs_client.client.HTTPError:
-            pass
+            if records.release.id == release_id_results:
+                pass
+
+            else:
+
+                try:
+
+                    album_data.folder = folder
+                    album_data.release_id = records.release.id
+                    album_data.release_url = records.release.url
+                    album_data.artist_id = records.release.artists[0].id
+                    album_data. artist_name = records.release.artists[0].name
+                    album_data.release_title = records.release.title
+                    album_data.artist_url = records.release.artists[0].url
+
+                    #album_data.genres = records.release.genres
+                    album_data.release_date = records.release.year
+
+                    if records.release.images[0]["uri"] is not None:
+                        album_data.release_image_url = records.release.images[0]["uri"]
+                    else:
+                        continue
+
+                    if records.release.master is not None:
+                        album_data.main_release_date = records.release.master.fetch("year")
+                        album_data.discogs_main_id = records.release.master.id
+                        album_data.discogs_main_url = records.release.master.url
+
+                    else:
+                        continue
+
+                    async with db_session.create_async_session() as session:
+                        session.add(album_data)
+                        await session.commit()
+
+                except discogs_client.client.HTTPError:
+                    pass
 
         print(records)
-
-        async with db_session.create_async_session() as session:
-            session.add(album_data)
-            await session.commit()
-
-
 
 
 async def update_db_data():
