0856f9c92d0342e40ddf803651873d6e2c320634
Paul Cutler <paul@paulcutler.org>
2022-06-03 21:31:24 -0500

    Working db import for main release info

--- services/admin_service.py
+++ services/admin_service.py
@@ -1,6 +1,3 @@
-from typing import Any
-
-from data.album_data import Album
 from sqlalchemy.future import select
 from data import db_session
 from data import config
@@ -10,6 +7,7 @@ from data.album_data import Album
 from data.genre_data import Genre
 from data.main_release_data import Main_Data
 from data.tracklist_data import Tracklists
+import sqlalchemy
 
 
 me = config.my_data.identity()
@@ -34,30 +32,30 @@ async def get_album_db_data():
 
             else:
 
-                try:
+                album_data.folder = folder_id
+                album_data.release_id = records.release.id
+                album_data.artist_id = records.release.artists[0].id
+                album_data.artist_name = records.release.artists[0].name
+                album_data.release_title = records.release.title
 
-                    album_data.folder = folder_id
-                    album_data.release_id = records.release.id
-                    album_data.release_url = records.release.url
-                    album_data.artist_id = records.release.artists[0].id
-                    album_data.artist_name = records.release.artists[0].name
-                    album_data.release_title = records.release.title
+                album_data.release_url = records.release.url
+                album_data.album_release_date = records.release.year
+
+                try:
                     album_data.artist_url = records.release.artists[0].url
 
-                    album_data.genres = records.release.genres[0]
-                    album_data.album_release_date = records.release.year
-
-                    if records.release.images[0]["uri"] is not None:
-                        album_data.release_image_url = records.release.images[0]["uri"]
-                    else:
-                        continue
-
-                    async with db_session.create_async_session() as session:
-                        session.add(album_data)
-                        await session.commit()
-
                 except discogs_client.client.HTTPError:
-                    pass
+                    album_data.artist_url = "None"
+
+                try:
+                    album_data.release_image_url = records.release.images[0]["uri"]
+
+                except TypeError:
+                    album_data.release_image_url = "None"
+
+                async with db_session.create_async_session() as session:
+                    session.add(album_data)
+                    await session.commit()
 
         print(records, records.release.year)
 
@@ -73,25 +71,45 @@ async def get_genre_data():
 async def get_main_release_data():
 
     async with db_session.create_async_session() as session:
-        release_id_query = select(Album.release_id).filter(Album.release_id)
+        release_id_query = select(Album.release_id)
         results = await session.execute(release_id_query)
 
-        release_id_results = results.scalar()
+        # Get all rows
+        release_id_results = results.all()
+        print(release_id_results[1], type(release_id_results[1]))
 
-        for records in release_id_results:
+
+        # Need to match the int from each row and pass it to the method to get the main data
+        for records in me.collection_folders[folder].releases:
             main_data = Main_Data()
 
-            if records.release.master is not None:
-                main_data.main_release_date = records.release.master.fetch("year")
-                main_data.discogs_main_id = records.release.master.id
-                main_data.discogs_main_url = records.release.master.url
+            try:
+
+                if records == me.collection_folders[folder].releases:
+                    print(records.release.id, " Already in db, pass")
+                    pass
+
+                else:
+                    main_data.release_id = records.release.id
+
+                try:
+                    main_data.main_release_date = records.release.master.fetch("year")
+                    main_data.discogs_main_id = records.release.master.id
+                    main_data.discogs_main_url = records.release.master.url
+
+                except AttributeError:
+                    #main_data.main_release_date = 1900
+                    pass
+
+                print("Adding to db", main_data.discogs_main_id)
 
                 async with db_session.create_async_session() as session:
                     session.add(main_data)
                     await session.commit()
 
-            else:
-                continue
+            except sqlalchemy.exc.IntegrityError:
+                print("Already in db, pass")
+                pass
 
 
 async def get_tracklist_data():
