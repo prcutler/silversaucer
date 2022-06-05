277f90c5e1b44b5aafe1e25eedc6a73f70f855e5
Paul Cutler <paul@paulcutler.org>
2022-06-05 18:19:35 -0500

    Add play-single to existing method for getting album data - and it works!

--- viewmodels/play/play_single_viewmodel.py
+++ viewmodels/play/play_single_viewmodel.py
@@ -1,8 +1,9 @@
 from typing import List, Optional
 
 from starlette.requests import Request
+from random import randint
 
-# from services.play_service import RandomRecordService
+from services import play_service, api_service
 from viewmodels.shared.viewmodel import ViewModelBase
 
 
@@ -10,12 +11,36 @@ class PlaySingleViewModel(ViewModelBase):
     def __init__(self, request: Request):
         super().__init__(request)
 
-        get_random_folder = RandomRecordService.single_random_folder()
-        # print(get_random_folder)
-        random_album_release_id = RandomRecordService.get_folder_count2(
-            get_random_folder
-        )
-        release_data = RandomRecordService.get_album_data(random_album_release_id)
+        self.release_id = None
+        self.release_url = None
+        self.artist_id = None
+        self.artist_url = None
+        self.artist_name = None
+        self.release_title = None
+        self.release_image_url = None
+        self.genres = None
+        self.album_release_date: Optional[str] = None
+        self.main_release_date: Optional[str] = None
+        self.track_title: Optional[List](str) = None
+        self.track_duration: Optional[List](str) = None
+        self.track_position: Optional[List](str) = None
+
+        self.login_status = self.is_logged_in
+        # print("Login status: ", self.login_status)
+
+    async def load(self):
+
+        random_folder = randint(1, 3)
+
+        if random_folder == 1:
+            folder = 2162486  # 10" folder
+        elif random_folder == 2:
+            folder = 2198941  # 12" folder
+        else:
+            folder = 2162483  # 7" folder
+
+        release_data = await play_service.get_album_data(folder)
+        # print("Release data: ", release_data)
 
         self.release_id = release_data.release_id
         self.release_url: Optional[str] = release_data.release_url
@@ -30,3 +55,5 @@ class PlaySingleViewModel(ViewModelBase):
         self.track_title: Optional[List](str) = release_data.track_title
         self.track_duration: Optional[List](str) = release_data.track_duration
         self.track_position: Optional[List](str) = release_data.track_position
+
+
