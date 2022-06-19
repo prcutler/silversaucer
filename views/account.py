d24472b0a95caf11f4d60775b2947044fa0063e1
Paul Cutler <paul@paulcutler.org>
2022-06-18 19:17:41 -0500

    Run black on views

--- views/account.py
+++ views/account.py
@@ -13,7 +13,7 @@ from viewmodels.account.login_viewmodel import LoginViewModel
 router = fastapi.APIRouter()
 
 
-@router.get('/account', include_in_schema=False)
+@router.get("/account", include_in_schema=False)
 @template()
 async def index(request: Request):
     vm = AccountViewModel(request)
@@ -37,10 +37,10 @@ async def index(request: Request):
 #    if vm.error:
 #        return vm.to_dict()
 
-    # Account creation
+# Account creation
 #    account = await user_service.create_account(vm.name, vm.email, vm.password)
 
-    # Login user
+# Login user
 #    response = fastapi.responses.RedirectResponse(url='/account', status_code=status.HTTP_302_FOUND)
 #    cookie_auth.set_auth(response, account.id)
 
@@ -49,15 +49,16 @@ async def index(request: Request):
 
 # ################### LOGIN #################################
 
-@router.get('/account/login', include_in_schema=False)
-@template(template_file='account/login.pt')
+
+@router.get("/account/login", include_in_schema=False)
+@template(template_file="account/login.pt")
 def login_get(request: Request):
     vm = LoginViewModel(request)
     return vm.to_dict()
 
 
-@router.post('/account/login', include_in_schema=False)
-@template(template_file='account/login.pt')
+@router.post("/account/login", include_in_schema=False)
+@template(template_file="account/login.pt")
 async def login_post(request: Request):
     vm = LoginViewModel(request)
     await vm.load()
@@ -71,15 +72,19 @@ async def login_post(request: Request):
         vm.error = "The account does not exist or the password is wrong."
         return vm.to_dict()
 
-    resp = fastapi.responses.RedirectResponse('/account', status_code=status.HTTP_302_FOUND)
+    resp = fastapi.responses.RedirectResponse(
+        "/account", status_code=status.HTTP_302_FOUND
+    )
     cookie_auth.set_auth(resp, user.id)
 
     return resp
 
 
-@router.get('/account/logout', include_in_schema=False)
+@router.get("/account/logout", include_in_schema=False)
 def logout():
-    response = fastapi.responses.RedirectResponse(url='/', status_code=status.HTTP_302_FOUND)
+    response = fastapi.responses.RedirectResponse(
+        url="/", status_code=status.HTTP_302_FOUND
+    )
     cookie_auth.logout(response)
 
     return response
